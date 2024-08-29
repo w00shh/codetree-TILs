f4945697b280862375d056283d741b6f88572f0f
Jung WooSeok <56007468+w00shh@users.noreply.github.com>
2024-08-29 21:17:40 +0900

    Title: 바이러스 검사, Time: 388ms, Memory: 7MB, Status: Passed - CodeTree

--- 240829/바이러스 검사/virus-detector.cpp
+++ 240829/바이러스 검사/virus-detector.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 long long N; // 식당
-long long customer[1000001]; // 각 식당 고객
+long long customer[1000005]; // 각 식당 고객
 long long can_search[2]; // 각 식당 팀장,팀원 최대 검색 고객 수
 
 void input(){
@@ -15,8 +15,8 @@ void input(){
     cin >> can_search[0] >> can_search[1];
 }
 
-int solve(){
-    int result = 0;
+long long solve(){
+    long long result = 0;
     for(int i=0;i<N;i++){
         customer[i] -= can_search[0];
         result += 1;
@@ -35,7 +35,7 @@ int solve(){
 
 int main() {
     input();
-    int result = solve();
+    long long result = solve();
     cout << result << "\n";
     return 0;
 }
